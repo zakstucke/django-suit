2bc0b63a03cc8ca2762be6254632784f79fb53ce
Mathieu Hinderyckx <mathieu@otainsight.com>
2022-09-01 10:25:01 +0200

    Updates

--- suit/tests/urls/__init__.py
+++ suit/tests/urls/__init__.py
@@ -1,25 +1,8 @@
-import django
-from django.conf.urls import include, url
+from django.urls import re_path
 from django.contrib import admin
 
 admin.autodiscover()
 
-try:
-    # Django 2.0+
-    from django.urls import re_path
-
-    urlpatterns = [
-        re_path(r'^admin/', admin.site.urls),
-    ]
-except ImportError:
-    try:
-        from django.conf.urls import patterns
-        urlpatterns = patterns(
-            '',
-            # Examples for custom menu
-            url(r'^admin/', include(admin.site.urls)),
-        )
-    except ImportError:  # Django 1.10+
-        urlpatterns = [
-            url(r'^admin/', include(admin.site.urls)),
-        ]
+urlpatterns = [
+    re_path(r'^admin/', admin.site.urls),
+]
